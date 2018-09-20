bf5ff726d8d74c2737187a696b94b0a91497605f
Dhiraj Upadhyaya <dup1966@gmail.com>
2018-09-20 09:56:57 +0530

    a

---
+++ 40-ts1/11-date1.R
@@ -0,0 +1,11 @@
+#Data Format in R 
+
+date1='15-Aug-1947'
+date1
+date2 = c('15-Aug-1947', "26-Jan-1950")
+date2
+
+class(date1) ; class(date2)
+#convert to Dates
+#Default Format of Date in R
+?as.Date()
