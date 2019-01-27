bb031980099522938ec273ab4c943777ca5329eb
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-01-27 13:36:56 +0530

    a

--- 2e3-LOGR-gre.R
+++ 2e3-LOGR-gre.R
@@ -9,47 +9,66 @@ head(inputData)
 dim(inputData)
 inputData
 summary(inputData)
+#sd of all columns 
+sd(inputData$admit)
+for (i in 1:4) { print(sd(inputData[,i]))}
+#use apply functions
 sapply(inputData, sd)
+lapply(inputData, sd)
+lapply(inputData, quantile)
+sapply(inputData, quantile)
+
+?sapply
+
 str(inputData)
 data= inputData  # make a copy for futher analysis
-
 data$rank = factor(data$rank)
 data$admit = factor(data$admit)
+
 str(data)
 ## 2way contingency table of cat outcome and predictors we want
 ## to make sure there are not 0 cells
 table(data$rank, data$admit)
-xtabs(~admit + rank, data = data)
-xtabs(~ gear + cyl + am , data=mtcars)
+#which rank of institute are more successful (in nos/ %) in getting admitted - 2 /1
+(t1= xtabs(~ admit + rank, data = data))
+addmargins(t1)
+prop.table(t1,2)
+?prop.table
+#xtabs(~ gear + cyl + am , data=mtcars)
 #create Logistic Model
-mylogit <- glm(admit ~ gre + gpa + rank, data = data, family = "binomial")
+mylogit = glm(admit ~ gre + gpa + rank, data = data, family = "binomial")
 
 summary(mylogit)
-#gre,gpa, rank are statistically significant, 
+#gre,gpa, rank are statistically significant, Star in each variable rows
 #For every one unit change in gre, the log odds of admission (versus non-admission) increases by 0.002.
-#For a one unit increase in gpa, the log odds of being admitted to graduate school increases by 0.804.
+#For a one unit increase in gpa, the log odds of being admitted to graduate school increases by 0.804. (keeping others constants)
 #The indicator variables for rank have a slightly different interpretation. For example, having attended an undergraduate institution with rank of 2, versus an institution with a rank of 1, changes the log odds of admission by -0.675.
 
 ## odds ratios only
 exp(coef(mylogit))
 library(dplyr)
-(ndata = sample_n(data, 3))
+(ndata1 = sample_n(data, 3))
+(p1=predict(mylogit,newdata=ndata1, type=c("response")))
+
 #Predict admit for input data
-(prob=predict(mylogit,newdata=ndata, type=c("response")))
-prob
-cbind(ndata, prob)
+(ndata2 = data.frame(gre=c(600, 650), gpa=c(2,3), rank=factor(c(1,2))))
+(p2= predict(mylogit,newdata=ndata2, type=c("response")))
 
+cbind(ndata1, p1, predict=ifelse(p1 < .5, 0, 1))
+cbind(ndata2, p2, predict=ifelse(p2 < .5, 0, 1))
 
+#predict for original values
+(p3 = predict(mylogit, newdata=data, type='response'))
+nrow(data)
+(predicted3 = factor(ifelse(p3 < .5, 0, 1)))
+(actual = data$admit)
+df = data.frame(predicted3, actual)
 #cutoff value
-library(InformationValue)
-(optCutOff <- optimalCutoff(data$admit, prob)[1] ) #.46
-confusionMatrix(data$admit, prob, threshold = optCutOff)
-(accuracy = (247+38)/ (sum(247+38+89+26))) # .715
-confusionMatrix(data$admit, prob, threshold = .7)
-(accuracy = (272+2)/ (sum(272+2+125+1))) #.685
-
-confusionMatrix(data$admit, prob, threshold = .2)
-
+head(df)
+df %>% mutate(same = predicted3 == actual)
+caret::confusionMatrix(df$predicted3, df$actual)
+(254+30)/(19+97+254+30)
+?confusionMatrix
 
 ## view data frame
 library(dplyr)
@@ -67,16 +86,50 @@ newdata1  #b=not admitted to institute
 
 library(caret)
 Index <- createDataPartition(y=data$admit, p=0.70, list=FALSE)
-head(Index)
+head(Index)  #rownumbers for T
+Index ; length(Index)
+length(Index)/ nrow(data)
+
 nrow(data)
-trainData = data[Index ,]
+trainData = data[Index , ]
+nrow(trainData)
 testData = data[-Index, ]
-table(data$admit); prop.table(table(data$admit))
-summary(trainData$admit); summary(testData$admit)
+nrow(testData)
 nrow(trainData) ; nrow(testData); nrow(trainData) + nrow(testData)
+table(data$admit); prop.table(table(data$admit)) #original table
 prop.table(table(trainData$admit))
 prop.table(table(testData$admit))
-#same promotion of admit in test and train
+summary(trainData$admit); summary(testData$admit)
+#proportion of Admit (0 and 1) are same/similar in train and test set
+#this is partition of data
+#trainData & testData
+log1 = glm(admit ~ gre + gpa + rank, data=trainData, family='binomial')
+summary(log1)
+#we will use all variables 
+#predict on test set 
+(predictNew = predict(log1, newdata = testData, type='response'))
+testData2 = cbind(testData, predictNew, predictClass = factor(ifelse(predictNew < .5, 0, 1)))
+head(testData2)
+caret::confusionMatrix(testData2$admit, testData2$predictClass)
+
 str(testData)
 #now construct a model with train and then test on testdata
 
+#logistic Regression
+
+
+
+library(InformationValue)
+(optCutOff <- optimalCutoff(data$admit, prob)[1] ) #.46
+confusionMatrix(data$admit, prob, threshold = optCutOff)
+(accuracy = (247+38)/ (sum(247+38+89+26))) # .715
+confusionMatrix(data$admit, prob, threshold = .7)
+(accuracy = (272+2)/ (sum(272+2+125+1))) #.685
+
+confusionMatrix(data$admit, prob, threshold = .2)
+
+##divide data set into Train and Test Set
+input <- mtcars[,c("am","cyl","hp","wt")]
+am.data = glm(formula = am ~ cyl + hp + wt, data = input, family = binomial)
+
+print(summary(am.data))
--- trg/iima-d4.R
+++ trg/iima-d4.R
@@ -16,16 +16,20 @@ summary(sleep)
 #logistic regression------
 #binary classification
 set.seed(1234)
-(price = sort(runif(100, 20,100)))
+(price = sort(runif(100, min=20, max=100)))
 (buy = factor(rep(c('Buy','NotBuy'), times=c(55,45))))
 purchase = data.frame(price, buy)
 head(purchase)
+purchase
+plot(x=price, y=buy, data=purchase)
+str(purchase)
+#log regression
 log_fit = glm(buy ~ price, data=purchase, family='binomial') 
 summary(log_fit)
-str(purchase)
-plot(x=price, y=buy, data=purchase)
+
 (ndata1= data.frame(price=c(30,50,70,90,98)))
 p1=predict(log_fit, newdata=ndata1, type='response')
+p1
 options(scipen=999)
 cbind(ndata1, p1)
 cbind(ndata1, p1, BUY=ifelse(p1 > .5,'No','Yes'))
