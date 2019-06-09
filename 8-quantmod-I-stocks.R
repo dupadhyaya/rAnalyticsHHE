f5000f2775a7e4dcf96f15b3f1a169957726037c
Dhiraj Upadhyaya <dup1966@gmail.com>
2019-06-09 18:34:49 +0530

    a

--- 8-quantmod-I-stocks.R
+++ 8-quantmod-I-stocks.R
@@ -11,8 +11,8 @@ if (!require("quantmod")) {
 
 #TS objects are 3 types in R : ts, zoo, xts
 
-start <- as.Date("2018-01-01")
-end <- as.Date("2019-02-09")
+start <- as.Date("2019-01-01")
+end <- as.Date("2019-06-31")
 getSymbols("SBIN.NS", src = "yahoo", from = start, to = end)
 ?getSymbols
 # What is SBI?
@@ -26,7 +26,7 @@ plot(tail(SBIN.NS[, "SBIN.NS.Close"]), main='Last 6 days rates')
 #plot candle Chart
 candleChart(SBIN.NS[1:50,], up.col = "black", dn.col = "red", theme = "white")
 
-
+?write.csv
 #ICICIBANK.NS
 #TATAMOTORS.NS
 getSymbols(c("ICICIBANK.NS", "TATAMOTORS.NS"), src = "yahoo", from = start, to = end)
@@ -43,3 +43,17 @@ legend("topright", c("SBIN", "ICICI", "TATATMOTORS"), lty = 1:3, col=1:3, cex = 
 #end of analysis
 # read this for more analysis :
 #https://cran.r-project.org/web/packages/quantmod/quantmod.pdf
+
+stock = SBIN.NS[,'SBIN.NS.Close']
+head(stock)
+class(stock)
+stock1=as.data.frame(stock)
+stock1$sdate = row.names(stock1)
+head(stock1)
+
+write.csv(stock1, 'data/stock1.csv', row.names=F)
+write.table(stock, 'data/stock1.txt', col.names=T)
+
+library(tibble)
+library(dplyr)
+stock %>% rownames_to_column('sdate')
